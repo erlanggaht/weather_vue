f23055f19b59c1794fb410c7861d3e2bac3a2cce
erlanggaht <erlanggahidayat.md@gmail.com>
2023-07-05 05:10:42 +0700

    oekh

--- src/utility/host_api.js
+++ src/utility/host_api.js
@@ -1,7 +1,7 @@
 const host_example = 'http://localhost:5173/data.json'
 
 function host_api(city){
-    const host_api = `http://api.weatherstack.com/current?access_key=cac8bb093c96fd3e9d7689e77fd90212&query=${city}`
+    const host_api = `https://api.weatherstack.com/current?access_key=cac8bb093c96fd3e9d7689e77fd90212&query=${city}`
 
     return host_api
 }
